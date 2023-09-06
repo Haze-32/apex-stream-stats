4263a8d0de81ef65f4a4609547c0273647f96e91
Haze <william.klus@gmail.com>
2023-09-06 12:13:51 -0400

    functionally working

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import staticData from './staticData.json';
-import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const db = getFirestore();
 
@@ -15,6 +15,25 @@ const Dashboard = () => {
             setUid(storedUser.uid);
         }
     }, []);
+
+
+    useEffect(() => {
+        // Fetch user settings from Firestore when uid is available
+        const fetchUserSettings = async () => {
+            if (uid) {
+                const docRef = doc(db, 'userSettings', uid);
+                const docSnap = await getDoc(docRef);
+
+                if (docSnap.exists()) {
+                    setCheckedStats(docSnap.data().checkedStats);
+                } else {
+                    console.log('No such document!');
+                }
+            }
+        };
+
+        fetchUserSettings();
+    }, [uid]);
 
     const handleCheckboxChange = (e) => {
         const { name, checked } = e.target;
@@ -57,7 +76,7 @@ const Dashboard = () => {
                         <div key={idx}>
                             <h3>{segment.metadata.name}</h3>
                             {Object.keys(segment.stats).map((statKey) => {
-                                const uniqueName = `${segment.type === 'overview' ? 'lifetime_' : ''}${statKey}`;
+                                const uniqueName = `${segment.type === 'overview' ? 'lifetime_' : 'legend_'}${statKey}`;
                                 return (
                                     <div key={uniqueName}>
                                         <input
@@ -75,10 +94,58 @@ const Dashboard = () => {
 
                     <div>
                         <h3>Selected Stats:</h3>
-                        {Object.keys(checkedStats).filter(key => checkedStats[key]).map(key => (
-                            <div key={key}>{key}: {checkedStats[key] ? segments.find(segment => segment.stats[key.replace('lifetime_', '')]).stats[key.replace('lifetime_', '')].displayValue : ''}</div>
-                        ))}
+
+                        {/* Lifetime Stats */}
+                        <div>
+                            <h4>Lifetime Stats:</h4>
+                            {Object.keys(checkedStats).filter(key => checkedStats[key] && key.startsWith('lifetime')).map(key => {
+                                const statKey = key.split('_')[1];
+                                const segment = segments.find(segment => segment.type === 'overview' && segment.stats[statKey]);
+
+                                return (
+                                    <div key={key}>
+                                        {statKey}: {segment ? segment.stats[statKey].displayValue : 'N/A'}
+                                    </div>
+                                );
+                            })}
+                        </div>
+
+                        {/* Legend Stats */}
+                        <div>
+                            {/* Create a temporary array that groups stats by legends */}
+                            {segments.filter(segment => segment.metadata.isActive === true).map((segment) => {
+
+                                // Filter keys that are checked and belong to this legend
+                                const filteredKeys = Object.keys(checkedStats).filter(
+                                    key => checkedStats[key] &&
+                                        key.startsWith('legend') &&
+                                        segments.find(s => s.metadata.name === segment.metadata.name && s.stats[key.split('_')[1]])
+                                );
+
+                                // If no keys for this legend are checked, do not display the legend title
+                                if (filteredKeys.length === 0) {
+                                    return null;
+                                }
+
+                                return (
+                                    <div key={segment.metadata.name}>
+                                        <h4 className="legendTitle">{segment.metadata.name}</h4>
+
+                                        {filteredKeys.map(key => {
+                                            const statKey = key.split('_')[1];
+                                            return (
+                                                <div key={key}>
+                                                    {statKey}: {segment.stats[statKey] ? segment.stats[statKey].displayValue : 'N/A'}
+                                                </div>
+                                            );
+                                        })}
+                                    </div>
+                                );
+                            })}
+                        </div>
+
                     </div>
+
 
                     <button type="submit">Submit</button>
                 </form>
--- src/components/UserStats.js
+++ src/components/UserStats.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getFirestore, doc, getDoc } from "firebase/firestore";  // Import Firestore methods
+import staticData from './staticData.json'; // Assuming you have access to the staticData.json file
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 const UserStats = () => {
     const { uid } = useParams();
     const [userData, setUserData] = useState(null);
-    const db = getFirestore(); // Initialize Firestore
+    const db = getFirestore();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -22,13 +23,36 @@ const UserStats = () => {
         fetchData();
     }, [uid, db]);
 
+    const segments = staticData.data.segments; // Assuming this is where you get the staticData from
+
     return (
         <div>
             <h1>User Stats for {uid}</h1>
             {userData ? (
                 <div>
-                    <strong>Field:</strong> {userData.checkedStats}
-                    {/* Add more fields as they are available */}
+                    {/* Lifetime Stats */}
+                    <div>
+                        <h4 className="legendTitle">Lifetime Stats:</h4>
+                        {Object.keys(userData.checkedStats).filter(key => userData.checkedStats[key] && key.startsWith('lifetime')).map(key => (
+                            <div key={key}>
+                                {key.split('_')[1]}: {segments.find(segment => segment.type === 'overview').stats[key.split('_')[1]].displayValue}
+                            </div>
+                        ))}
+                    </div>
+
+                    {/* Legend Stats */}
+                    <div>
+                        <h4 className="legendTitle">Legend Stats:</h4>
+                        {Object.keys(userData.checkedStats).filter(key => userData.checkedStats[key] && key.startsWith('legend')).map(key => {
+                            const statKey = key.split('_')[1];
+                            const segment = segments.find(segment => segment.metadata.isActive === true && segment.stats[statKey]);
+                            return (
+                                <div key={key}>
+                                    {segment.metadata.name} - {statKey}: {segment ? segment.stats[statKey].displayValue : 'N/A'}
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             ) : (
                 <p>Loading or no data available...</p>
